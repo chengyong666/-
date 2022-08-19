ac029f3c527bbdbdb3dceedf578f5c6f28e6b2c7
程大雍 <837095241@qq.com>
2022-08-19 16:18:43 +0800

    frist commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,18 @@
+<template></template>
+
+<script  lang="ts">
+import { watch, provide, defineComponent, ref, reactive, toRefs } from "vue";
+import { mapState, mapMutations } from "vuex";
+
+export default defineComponent({
+  name: "",
+  components: {},
+  computed: {},
+  setup(props, contex) {
+    return {};
+  },
+});
+</script>
+
+<style scoped>
+</style>
---
+++ src/views/son.vue
@@ -0,0 +1,14 @@
+<template></template>
+
+<script  lang="ts">
+import { inject, defineComponent, ref, reactive, toRefs } from "vue";
+export default defineComponent({
+  name: "",
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped>
+</style>

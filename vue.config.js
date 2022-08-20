9bab6a762440888329bb5dce6ae780ce71f5c37a
程大雍 <837095241@qq.com>
2022-08-20 21:46:30 +0800

    vip

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,9 @@
 module.exports = {
 
   lintOnSave: false,
-  publicPath: '/'
+  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
+  outputDir: 'dist',
+  indexPath: 'index.html',
+  lintOnSave: false,
+  transpileDependencies: true,
 }

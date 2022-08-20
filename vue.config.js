4e350a2008999c1ffe294a01e9af7dd63c458231
程大雍 <837095241@qq.com>
2022-08-20 21:56:31 +0800

    vip

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,8 @@
-
-module.exports = {
-
-  lintOnSave: false,
-  publicPath: process.env.NODE_ENV === 'production' ? './' : './',
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
   outputDir: 'dist',
   indexPath: 'index.html',
   lintOnSave: false,
   transpileDependencies: true,
-}
+})
